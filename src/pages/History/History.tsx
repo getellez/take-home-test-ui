99398f9a69dd92ec7ed9f9c14de20fc384f975ba
German Tellez <gertellezv@gmail.com>
2022-10-19 16:35:13 -0500

    Get commits from API and add Footer component

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,7 +1,25 @@
-import './History.css'
+import { useEffect, useState } from 'react';
 import { CommitsList } from '../../components/CommitsList/CommitsList';
+import { Footer } from '../../components/Footer/Footer';
+import axios from 'axios';
+import './History.css'
 
 export const History = () => {
+  
+  const [commits, setCommits] = useState([])
+  const getCommitsFromApi = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:3001/api/v1/commits')
+      setCommits(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getCommitsFromApi()
+  })
+  
   return (
     <div className='History'>
       <div className="History__container">
@@ -10,8 +28,8 @@ export const History = () => {
           <p className='History__description'>The purpose of this app is to list the commits of the project</p>
         </div>
 
-        <CommitsList />
-
+        <CommitsList commits={commits} />
+        <Footer />
 
       </div>
 
