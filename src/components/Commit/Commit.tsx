60b1c0a14dd8ab61728f9ca64f82ef72a3d1b3a1
German Tellez <gertellezv@gmail.com>
2022-10-19 16:30:05 -0500

    Add Commit component and Commit Interface

--- src/components/Commit/Commit.tsx
+++ src/components/Commit/Commit.tsx
@@ -2,17 +2,25 @@ import React from 'react'
 import { Card, CardContent, Grid } from '@mui/material';
 
 import './Commit.css'
+import { Commit as ICommit } from '../../interfaces/commit.interfaces';
 
-export const Commit = () => {
+
+
+export const Commit = ({ sha, author, date, message }: ICommit) => {
+  
+  const customDate = new Date(date).toLocaleString()
+
   return (
     <Grid item xs={10} lg={8}>
       <div className="Commit">
         
         <Card variant="outlined">
           <CardContent>
-            <p>This is a commit message</p>
+            <p className='Commit__message'>{message}</p>
             <small>
-              <code>SHA: aslñdknaslñkdnaslkdmnaldfnaslfn</code>
+              <p>Author: {author}</p>
+              <p>Pushed at: {customDate}</p>
+              <code>SHA: {sha}</code>
             </small>
           </CardContent>
         </Card>
