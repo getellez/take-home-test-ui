d45fe45e0c3a02e2551bf4dfb50ca3698676a318
German Tellez <gertellezv@gmail.com>
2022-10-19 20:09:09 -0500

    Add react router and Error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import { Error } from './pages/Error/Error';
 import { History } from './pages/History/History';
 
 function App() {
   return (
-    <>
-      <History />
-    </>
+    <Routes>
+      <Route path='' element={<History />} />
+      <Route path='/*' element={<Error />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Error.css'
+
+export const Error = () => {
+  return (
+    <div className='Error'>
+      <h1>Oops!</h1>
+      <h2>You're not supposed to be here</h2>
+    </div>
+  )
+}
